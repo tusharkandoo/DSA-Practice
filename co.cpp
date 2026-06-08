d393d09cf0c1c4e7c7382206a9812a12f96d9818
tusharkandoo <tusharkandoo@gmail.com>
2026-06-09 00:21:07 +0530

    Remove remaining files from root directory - successfully moved to their respective folders

--- co.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstring>
-using namespace std;
-int main(){
-    char str1[]= "hello";
-    char str2[]= "world";
-    cout<< strcmp(str1,str2)<<endl;
-    return 0;
-}
--- len.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include<cstring>
-using namespace std;
-int main(){
-    char str1[]= "hello";
-    cout<<strlen(str1)<<endl;
-    return 0;
-}
