cfb3e2b2ce9373e8ce7e9fd0f3e4790243d233e2
tusharkandoo <tusharkandoo@gmail.com>
2026-06-10 03:26:57 +0530

    made some changes and the time complexity decreased

--- SOrting/bubble.cpp
+++ SOrting/bubble.cpp
@@ -1,22 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+        cin>> arr[i];
     }
-    for(int i=n-1;i>0;i--){
-        for(int j=0;j<i;j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+    for (int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            swap( arr[j],arr[j+1]);
+
         }
     }
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<< arr[i]<<" ";
     }
 }
