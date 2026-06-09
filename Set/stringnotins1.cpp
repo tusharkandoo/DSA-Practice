7ae87e47404720edff2fd9bae691d16a062ac999
tusharkandoo <tusharkandoo@gmail.com>
2026-06-10 03:41:09 +0530

    fixed the syntax error of the code

--- Set/stringnotins1.cpp
+++ Set/stringnotins1.cpp
@@ -1,24 +1,35 @@
-//WAP to give the elements which are present in s2 but not in s1
+// WAP to give the elements which are present in s2 but not in s1
 
 #include <bits/stdc++.h>
 using namespace std;
-set <char> s1;
-set <char> s2;
-char x;
-int N,M;
-cin>>N>>M;
-for (int i=1;i<=N;i++){
-    cin>>x;
-    s1.insert(x)
-}
-for (int i=1;i<=M;i++){
-    cin>>x;
-    s2.insert(x);
-}
-set <char> s3;
-for (int v:s2){
-    if(s1.find(v)==s1.end()){
-        s3.insert(v);
+
+int main() {
+    set<char> s1, s2, s3;
+    char x;
+    int N, M;
+
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> x;
+        s1.insert(x);
     }
+
+    for (int i = 1; i <= M; i++) {
+        cin >> x;
+        s2.insert(x);
+    }
+
+    for (char v : s2) {
+        if (s1.find(v) == s1.end()) {
+            s3.insert(v);
+        }
+    }
+
+    cout << "Elements present in s2 but not in s1: ";
+    for (char v : s3) {
+        cout << v << " ";
+    }
+
+    return 0;
 }
- 
